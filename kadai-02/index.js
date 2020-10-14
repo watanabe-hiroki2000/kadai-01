7f7588688a38b6855e25b745c27c16f701974bd3
watanabe-hiroki2000 <hiroki00watanabe12rits@gmail.com>
2020-10-14 05:38:45 +0000

    課題2を追加

---
+++ kadai-02/index.js
@@ -0,0 +1,19 @@
+// 文字列を結合して表示する
+const firstName = 'Hiroki'
+const familyName = 'Watanabe'
+
+console.log(firstName + familyName)
+
+// 1から10までの数字の和を表示する
+console.log(1+2+3+4+5+6+7+8+9+10)
+
+// 真偽型の演算
+const dog = false
+const cat = false
+// 犬または猫を飼っている
+console.log(dog || cat)
+// 犬と猫の両方を飼っている
+console.log(dog && cat)
+//犬も猫も飼っていない
+console.log(!dog && !cat)
+console.log(!(dog || cat))
---
+++ untitle/index.js
@@ -0,0 +1,14 @@
+ //数字　n　が　偶数である
+ //数字　n　が　3の倍数である
+
+ const n = 9
+ const isGusu = false
+ const Is3x = true
+
+ //数字　n　が　偶数である　かつ　3の倍数である　=>　AND　演算
+ isGusu　&&　is3x //false && true // false
+
+ //　数字 n が　偶数である　または　3の倍数である　=>　OR 演算
+ isGusu || is3x // false || true === true
+
+ // 数字 n が　偶数でない　
