4e46a534fe7fb1b8d057d82a16d60fda1d0a12ce
watanabe-hiroki2000 <hiroki00watanabe12rits@gmail.com>
2020-10-28 05:28:41 +0000

    課題3を追加

--- kadai-03/index1.js
+++ kadai-03/index1.js
@@ -1,8 +1,8 @@
 //生まれた日を入力
 const day = parseInt(process.argv[2])
-if (/day % 3 === 0){
+if (day % 3 === 0){
     console.log('あなたはsquareな性格です')
-} else if (/* 条件式 */){
+} else if (day % 3 === 1){
     console.log('あなたはsmallな性格です')
 } else {
     console.log('あなたはbigな性格です')
