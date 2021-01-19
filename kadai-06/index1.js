c02c58c54ab59b23064f35d1272581958336860c
watanabe-hiroki2000 <hiroki00watanabe12rits@gmail.com>
2021-01-19 13:57:29 +0000

    課題6を追加

--- kadai-06/index1.js
+++ kadai-06/index1.js
@@ -1,6 +1,15 @@
 const obj1 = "abc"
 const obj2 = 123
-const obj3 =
+const obj3 = false 
+const obj4 = { a: 123 }
+const obj5 = { func: () => console.log('Hi!') }
+const obj6 = { func: () => console.log('Hi!'), a: 123 }
+const obj7 = { property: undefined }
 
 console.log(JSON.stringfy(obj1))
 console.log(JSON.stringfy(obj2))
+console.log(JSON.stringfy(obj3))
+console.log(JSON.stringfy(obj4))
+console.log(JSON.stringfy(obj5))
+console.log(JSON.stringfy(obj6))
+console.log(JSON.stringfy(obj7))
