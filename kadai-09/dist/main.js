158d0ad29fa23e59dc704a4108e8dd7f3f76e25e
watanabe-hiroki2000 <hiroki00watanabe12rits@gmail.com>
2021-01-19 16:38:29 +0000

    課題9を追加

---
+++ kadai-09/dist/main.js
@@ -0,0 +1,1 @@
+(()=>{var o={902:o=>{o.exports=()=>{console.log("hello, a!")}},78:o=>{o.exports=()=>{console.log("hello, b!")}}},e={};function r(t){if(e[t])return e[t].exports;var s=e[t]={exports:{}};return o[t](s,s.exports,r),s.exports}(()=>{const o=r(902),e=r(78);o(),e()})()})();
